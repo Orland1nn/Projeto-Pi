bc2134ee6cf9a9cb4152b1f5a89a48483f64a09b
Luis Felipe Gomes <luisfelipegomes124@gmail.com>
2025-09-29 16:21:53 -0300

    [#17] Back Atualização.

--- Adega/Back/src/produto/produto.service.ts
+++ Adega/Back/src/produto/produto.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Produto } from './produto.entity';
@@ -18,5 +18,21 @@ export class ProdutoService {
 
   async listar(): Promise<Produto[]> {
     return await this.produtoRepository.find();
+  }
+
+  async atualizarPorNome(
+    nome: string,
+    dados: Partial<CreateProdutoDto>,
+  ): Promise<Produto> {
+    const produto = await this.produtoRepository.findOne({
+      where: { nome },
+    });
+
+    if (!produto) {
+      throw new NotFoundException('Produto não encontrado');
+    }
+
+    Object.assign(produto, dados);
+    return this.produtoRepository.save(produto);
   }
 }
